853fa21c68f57a367a6cbb43f2f2bf30a5ec499c
Monoget <monoget2@gmail.com>
2022-09-01 17:14:09 +0600

    update review

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,31 @@ $('#owl-carousel').owlCarousel({
     },
 })
 
+
+let SwiperTop = new Swiper('.swiper--top', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    speed: 6000,
+    autoplay: {
+        delay: 1,
+    },
+    loop: true,
+    slidesPerView:'auto',
+    allowTouchMove: false,
+    disableOnInteraction: true
+});
+
+let SwiperBottom = new Swiper('.swiper--bottom', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    speed: 6000,
+    autoplay: {
+        delay: 1,
+        reverseDirection: true
+    },
+    loop: true,
+    loopedSlides: 15,
+    slidesPerView:'auto',
+    allowTouchMove: false,
+    disableOnInteraction: true
+});
